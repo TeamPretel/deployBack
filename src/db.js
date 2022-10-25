e79fe0479be0c8722c737a5eda29b9dc8f385915
Javier Gutiérrez <dukintosh@gmail.com>
2022-10-25 05:55:49 -0500

    asdfasdf

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize')
 const fs = require('fs')
 const path = require('path')
 
-
+// conf rara
 // const {
 //   DB_USER, DB_PASSWORD, DB_HOST
 // } = process.env;
